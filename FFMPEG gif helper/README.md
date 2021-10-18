d2a30a786f3ff7ed3e087811942764ac635e1e1e
Owen Quinlan <46948241+BuyMyMojo@users.noreply.github.com>
2021-10-19 01:23:16 +1100

    Added FFMPEG gif helper

---
+++ FFMPEG gif helper/README.md
@@ -0,0 +1,11 @@
+# FFMPEG Gif helper
+
+This is a tool I made to easily convert videos to gifs with unique pallets
+
+## HD_GifGen.sh
+
+This generates a gif with a single pallet for the entire gif
+
+## UHD_GifGen.sh
+
+This generates a gif with a pallet for every frame of the gif
---
+++ FFMPEG gif helper/UHD_GifGen.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+Help()
+{
+   # Display Help
+   echo "This script converts any input supported by ffmpeg into a gif with a new pallet every frame."
+   echo
+   echo "Syntax: UHD_GifGen.sh [-h] /Path/To/Video"
+   echo "options:"
+   echo "h     Print this Help."
+   echo
+}
+
+while getopts ":h" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+   esac
+done
+
+InFile=$1
+
+eval $(ffprobe -v quiet -show_format -of flat=s=_ -show_entries stream=height,width,nb_frames,duration,codec_name $1);
+DurationInSeconds=${streams_stream_0_duration}
+fps=$(awk '{print $1/$2}' <<<"${streams_stream_0_nb_frames} $DurationInSeconds")
+
+if (( $fps > 50 ));
+then
+    ffmpeg -i $1 -c:v gif -filter_complex "[0:v] fps=50,split [a][b];[a] palettegen=stats_mode=single:reserve_transparent=off [p];[b][p] paletteuse=new=1" "$1_conv.gif"
+else
+    ffmpeg -i $1 -c:v gif -filter_complex "split [a][b];[a] palettegen=stats_mode=single:reserve_transparent=off [p];[b][p] paletteuse=new=1" "$1_conv.gif"
+fi
